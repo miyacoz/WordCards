68e8995a8bc38bd4a7e2fbacfb0441928c59cb9d
miyacoz <z@zeppel.biz>
2020-10-31 03:11:12 +0900

    added mongodb connection for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import * as Dotenv from 'dotenv'
 import * as Fs from 'fs'
 import * as Http from 'http'
 // import * as HttpProxy from 'http-proxy'
+import { MongoClient, Db as MongoDb } from 'mongodb'
 
 const dotenvResult: Dotenv.DotenvConfigOutput = Dotenv.config()
 if (dotenvResult.error) {
@@ -15,7 +16,41 @@ const serverOptions: Https.ServerOptions = {
   cert: Fs.readFileSync(config?.SSL_CERT || '')
 }
 
-const SERVER_PORT = 8888
+const SERVER_PORT: number = Number(config?.SERVER_PORT) || 0
+
+const DB_URL: string = `mongodb://${config?.MONGO_HOST || ''}:${config?.MONGO_PORT || 0}/`
+const DB_NAME: string = 'wordcards'
+
+let db: MongoDb;
+
+const connectDb = async (): Promise<void> => {
+  try {
+    const client: MongoClient = await MongoClient.connect(DB_URL, {
+      useUnifiedTopology: true,
+      auth: {
+        user: config?.MONGO_USER || '',
+        password: config?.MONGO_PASS || '',
+      }
+    })
+      
+    console.info('mongo ok')
+
+    db = client.db(DB_NAME)
+
+    const collections: { name: string; type: string }[] = await db.listCollections({}, {
+      nameOnly: true,
+    }).toArray()
+    const collectionNames: string[] = collections.map(v => v.name)
+
+    if (!collectionNames.includes('words')) {
+      db.createCollection('words').then(() => {
+        console.info('collection created')
+      })
+    }
+  } catch (e) {
+    console.warn(e)
+  }
+}
 
 const routes: Http.RequestListener = (q, r): void => {
   q.on('readable', () => {
@@ -31,15 +66,18 @@ const routes: Http.RequestListener = (q, r): void => {
     'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
   }
   r.writeHead(200, headers)
+
+  const now = new Date()
   const data = {
     data: {
       it: 'good'
     },
-    now: new Date()
+    now
   }
-  r.write(JSON.stringify(data))
+  r.write(Number(now) % 2 ? JSON.stringify(data) : '')
   r.end()
 }
 
+connectDb()
 Https.createServer(serverOptions, routes).listen(SERVER_PORT)
-console.log('i\'m running now')
+console.info('i\'m running now')
