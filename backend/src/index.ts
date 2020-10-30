35438964364b57c8b5ca8845768f3dbeaa242367
miyacoz <z@zeppel.biz>
2020-10-31 03:57:37 +0900

    extracted DB as a module

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,8 @@ import { config as DotenvConfig, DotenvConfigOutput, DotenvParseOutput } from 'd
 import * as Fs from 'fs'
 import * as Http from 'http'
 // import * as HttpProxy from 'http-proxy'
-import { MongoClient, Db as MongoDb } from 'mongodb'
+
+import DB from './DB'
 
 const dotenvResult: DotenvConfigOutput = DotenvConfig()
 if (dotenvResult.error) {
@@ -23,50 +24,6 @@ const serverOptions: Https.ServerOptions = {
 
 const SERVER_PORT: number = Number(config?.SERVER_PORT) || 0
 
-class DB {
-  private db: MongoDb | null = null
-
-  private config: DotenvParseOutput
-
-  private DB_URL: string
-
-  private DB_NAME: string = 'wordcards'
-
-  public constructor(config: DotenvParseOutput) {
-    this.config = config
-    this.DB_URL = `mongodb://${config.MONGO_HOST || ''}:${config.MONGO_PORT || 0}/`
-    this.connect()
-  }
-
-  public connect = async (): Promise<void> => {
-    try {
-      const client: MongoClient = await MongoClient.connect(this.DB_URL, {
-        useUnifiedTopology: true,
-        auth: {
-          user: this.config.MONGO_USER || '',
-          password: this.config.MONGO_PASS || '',
-        }
-      })
-        
-      console.info('mongo ok')
-
-      this.db = client.db(this.DB_NAME)
-
-      const collections: { name: string; type: string }[] = await this.db.listCollections({}, {
-        nameOnly: true,
-      }).toArray()
-      const collectionNames: string[] = collections.map(v => v.name)
-
-      if (!collectionNames.includes('words')) {
-        this.db.createCollection('words').then(() => {
-          console.info('collection created')
-        })
-      }
-    } catch (e) {
-      console.warn(e)
-    }
-  }
-}
 if (config) {
   new DB(config)
 }
